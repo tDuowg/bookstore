8b4cf9942afc474262bc20b0c61b8bcc820cd965
thuyduong08 <dnmvvn08@gmail.com>
2025-03-21 21:21:19 -0400

    update

--- login/login.js
+++ login/login.js
@@ -2,6 +2,18 @@ const btnLogin = document.getElementById("btnLogin");
 const email = document.getElementById("floatingInput");
 const password = document.getElementById("floatingPassword");
 function handleLogin() {
+    if (!email.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+        alert("Invalid email format");
+        return;
+    }
+    if (password.value.length < 6) {
+        alert("Password must contain at least 6 characters");
+        return;
+    }
+    if (!password.value.match(/[a-z]/) || !password.value.match(/[A-Z]/) || !password.value.match(/[0-9]/)) {
+        alert("Password must contain at least 1 lowercase letter, 1 uppercase letter and 1 number");
+        return;
+    }
     let users = localStorage.getItem("users");
     if (!email.value || !password.value) {
         alert("Please enter email and password");
--- signup/signup.js
+++ signup/signup.js
@@ -6,6 +6,34 @@ const confirmPassword = document.getElementById("confirmPassword");
 const gender = document.getElementById("gender");
 const birth = document.getElementById("birth");
 function handleSignUp() {
+    if (fullName.value.split(" ").length < 2) {
+        alert("Please enter full name with at least 2 words");
+        return;
+    }
+    if (fullName.value.match(/[^a-zA-Z ]/)) {
+        alert("Full name must contain only letters");
+        return;
+    }
+    if (!email.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+        alert("Invalid email format");
+        return;
+    }
+    if (password.value.length < 6) {
+        alert("Password must contain at least 6 characters");
+        return;
+    }
+    if (!password.value.match(/[a-z]/) || !password.value.match(/[A-Z]/) || !password.value.match(/[0-9]/)) {
+        alert("Password must contain at least 1 lowercase letter, 1 uppercase letter and 1 number");
+        return;
+    }
+    if (confirmPassword.value !== password.value) {
+        alert("Password and confirm password do not match");
+        return;
+    }
+    if (!gender.value || !birth.value) {
+        alert("Please enter input");
+        return;
+    }
     let users = JSON.parse(localStorage.getItem('users')) || [];
     if (!fullName.value || !email.value || !password.value || !confirmPassword.value) {
         alert("Please enter username and password");
